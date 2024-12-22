16dc0763cc555816a4374f42a6875b3d6c47b7fd
sebastianjnuwu <sebastianjnuwu@gmail.com>
2024-12-21 22:00:08 -0300

    v1.0.0

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,22 +1,19 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
- // https://capacitorjs.com/docs/config
+// https://capacitorjs.com/docs/config
 const config: CapacitorConfig = {
-  appId: 'uwu.cookie.app',
-  appName: 'Cookie',
-  webDir: 'www',
-  bundledWebRuntime: false,
-  plugins: {
-    AdMob: {
-      androidAppId: 'ca-app-pub-6690516270288705~6535066913',
-      maxAdContentRating: 'G', 
-      tagForChildDirectedTreatment: false, 
-      tagForUnderAgeOfConsent: false,
+    appId: 'uwu.cookie.app',
+    appName: 'Cookie',
+    webDir: 'www',
+    bundledWebRuntime: false,
+    plugins: {
+        AdMob: {
+            androidAppId: 'ca-app-pub-6690516270288705~6535066913',
+            maxAdContentRating: 'G',
+            tagForChildDirectedTreatment: false,
+            tagForUnderAgeOfConsent: false,
+        }
     },
-    PushNotifications: {
-     presentationOptions: ["badge", "sound", "alert"],
-    },
-  },
 };
 
 export default config;
