8086ea42bacbce5010642bdc849b9d5a912cc3a4
sebastianjnuwu <sebastianjnuwu@gmail.com>
2024-12-23 09:45:19 -0300

    feat: integrate push notifications and refactor AdMob plugin

---
+++ www/res/plugins/push-notifications.ts
@@ -0,0 +1,36 @@
+import { PushNotifications } from '@capacitor/push-notifications';
+
+const register = async () => {
+
+  let STATUS = await PushNotifications.checkPermissions();
+  console.log(STATUS);
+
+  if (STATUS.receive === 'prompt') {
+    STATUS = await PushNotifications.requestPermissions();
+    console.log(STATUS);
+  }
+
+  if (STATUS.receive !== 'granted') {
+    console.log('not permission');
+  }
+
+  await PushNotifications.register();
+  
+  PushNotifications.addListener('registration', (token) => {
+    console.log(token);
+  });
+
+  PushNotifications.addListener('registrationError', (error) => {
+    console.log(error);
+  });
+
+  PushNotifications.addListener('pushNotificationReceived', (notification) => {
+    console.log(notification);
+  });
+
+  PushNotifications.addListener('pushNotificationActionPerformed', (action) => {
+    console.log(action);
+  });
+};
+
+register();
