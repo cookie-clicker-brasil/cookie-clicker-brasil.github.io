b895a0473ea44e4813bc5b8b890b0a14ae4b0079
sebastianjnuwu <sebastianjnuwu@gmail.com>
2024-12-23 09:52:59 -0300

    v1.0.0

--- www/res/plugins/push-notifications.ts
+++ www/res/plugins/push-notifications.ts
@@ -1,36 +1,35 @@
-import { PushNotifications } from '@capacitor/push-notifications';
+import { PushNotifications } from "@capacitor/push-notifications";
 
 const register = async () => {
+	let STATUS = await PushNotifications.checkPermissions();
+	console.log(STATUS);
 
-  let STATUS = await PushNotifications.checkPermissions();
-  console.log(STATUS);
+	if (STATUS.receive === "prompt") {
+		STATUS = await PushNotifications.requestPermissions();
+		console.log(STATUS);
+	}
 
-  if (STATUS.receive === 'prompt') {
-    STATUS = await PushNotifications.requestPermissions();
-    console.log(STATUS);
-  }
+	if (STATUS.receive !== "granted") {
+		console.log("not permission");
+	}
 
-  if (STATUS.receive !== 'granted') {
-    console.log('not permission');
-  }
+	await PushNotifications.register();
 
-  await PushNotifications.register();
-  
-  PushNotifications.addListener('registration', (token) => {
-    console.log(token);
-  });
+	PushNotifications.addListener("registration", (token) => {
+		console.log(token);
+	});
 
-  PushNotifications.addListener('registrationError', (error) => {
-    console.log(error);
-  });
+	PushNotifications.addListener("registrationError", (error) => {
+		console.log(error);
+	});
 
-  PushNotifications.addListener('pushNotificationReceived', (notification) => {
-    console.log(notification);
-  });
+	PushNotifications.addListener("pushNotificationReceived", (notification) => {
+		console.log(notification);
+	});
 
-  PushNotifications.addListener('pushNotificationActionPerformed', (action) => {
-    console.log(action);
-  });
+	PushNotifications.addListener("pushNotificationActionPerformed", (action) => {
+		console.log(action);
+	});
 };
 
 register();
