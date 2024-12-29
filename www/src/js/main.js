f0297b958601b25f02000b18ac0b1606c57c28d0
sebastianjnuwu <sebastianjnuwu@gmail.com>
2024-12-29 12:18:49 -0300

    feat(styles): UI

--- www/src/js/main.js
+++ www/src/js/main.js
@@ -1,9 +1,19 @@
 $(() => {
+  
   $("splash-screen").on("click", () => {
+    $("#splash")[0].volume = 0.1;
     $("#splash")[0].play();
     setTimeout(() => {
       $("splash-screen").remove();
-      $("ui").show();
+      $("start-screen").show();
     }, 1000);
   });
+  
+  $("start-screen button").on("click", () =>  $("#click")[0].play());
+  
+  $("#play").on("click", () => {
+    $("start-screen").remove();
+    $("ui").show();
+  });
+  
 });
