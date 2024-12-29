77867cbfc373c28efd54f31cd5783476953dcc2a
sebastianjnuwu <sebastianjnuwu@gmail.com>
2024-12-28 22:25:00 -0300

    feat(styles): update styles

--- www/src/js/cookie/game.js
+++ www/src/js/cookie/game.js
@@ -1,8 +1,25 @@
+let cookies = 0;
+let $cps = 0;
+
+const $update_cps = () => {
+  $('#cps').text($cps); 
+  $cps = 0;  
+};
+
+const $update_cookies = () => {
+  cookies++; 
+  $cps++;
+  $('#click-cookie').text(`${cookies}`);
+  localStorage.setItem('cookie', cookies);
+};
+
+setInterval($update_cps, 1000);
+
 $('.cookie').on('click', function (e) {
-  
-  const a = 1;
-  
-  const $effect = $(`<div class="click-effect">+${a}</div>`);
+
+  $update_cookies();
+
+  const $effect = $(`<div class="click-effect">+1</div>`);
 
   const offset = $(this).offset();
   const X = e.pageX - offset.left;
