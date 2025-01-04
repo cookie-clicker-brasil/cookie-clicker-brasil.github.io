252c64c36001dabc49d4109231b9b05f3c6eb1c5
sebastianjnuwu <sebastianjnuwu@gmail.com>
2025-01-04 15:06:06 -0300

    feat(socket): add script 1/3

--- www/src/js/main.js
+++ www/src/js/main.js
@@ -12,43 +12,16 @@ $(() => {
 
   $("start-screen button").on("click", () => $("#click")[0].play());
 
-
-  $('input[name="gameOption"]').on('change', () => {
-    if ($('#joinRoom').is(':checked')) {
-      $('#roomCodeContainer').show();
-      $("#gameTimeContainer").hide();
+  $('input[name="option_game"]').on('change', () => {
+    if ($('#room_join').is(':checked')) {
+      $('#room_code').val("");
+      $('#code_container').show();
+      $("#game_container").hide();
     } else {
-      $('#roomCodeContainer').hide();
-      $("#gameTimeContainer").show();
+      $('#room_code').val("");
+      $('#code_container').hide();
+      $("#game_container").show();
     }
   });
 
-  // Captura os dados do formulário
-  $('#gameSettingsForm').on('submit', function (e) {
-    e.preventDefault();
-
-    const nickname = $('#nickname').val();
-    const gameOption = $('input[name="gameOption"]:checked').val();
-    const roomCode = $('#roomCode').val();
-
-    if (gameOption === 'join' && !roomCode) {
-      alert('Por favor, insira o código da sala.');
-      return;
-    }
-
-    // Processar os dados (exemplo: enviar para o backend)
-    console.log({
-      nickname,
-      gameOption,
-      roomCode: gameOption === 'join' ? roomCode : null,
-    });
-
-    // Fechar o modal após a confirmação
-    $('#gameModal').modal('hide');
-  });
-
-  $("#play").on("click", () => {
-    // $("start-screen").remove();
-    // $("ui").show();
-  });
 });
--- www/src/js/socket.js
+++ www/src/js/socket.js
@@ -1,39 +1,77 @@
-const socket = io("http://localhost:3000", {
-  transports: ["websocket", "polling"],
-});
-
 const $countdown = $("#countdown-container");
 const countdown_text = $("#countdown");
 const code = $("#current_code");
 const online = $("#online");
+const socket = io("http://localhost:3000", {
+  transports: ["websocket", "polling"],
+});
 
-socket.emit("room", {
-  code: "M7ZGS5",
-  nickname: "sebastianjnuwu",
+if (Cookies.get('nickname')) {
+ $('#nickname').val(Cookies.get('nickname'));
+};
+
+socket.on("connect", () => {
+  socket.emit("rejoin_room", {
+    room_player: Cookies.get('nickname'), 
+    room_code: Cookies.get('code'), 
+  });
+});
+
+  // leave the room spontaneously!
+$("#leave_room").on("click", () => {
+  
+  socket.emit("leave_room", {
+    room_code: Cookies.get('code'),
+    room_player: Cookies.get('nickname')
+  });
+  
+  $("ui").hide();
+  $("#start-screen").show();
+  
+});
+
+$('#form_room').on('submit', (e) => {
+  
+  e.preventDefault();
+
+  const $nickname = $('#nickname').val();
+  const $code = $('#room_code').val();
+  const $time = $('#game_time').val();
+  
+  Cookies.set('nickname', $nickname);
+ 
+  socket.emit("room", {
+    room_code: $code, 
+    room_time: $time || 3, 
+    room_player: $nickname
+  });
 });
 
 socket.on("error", (err) => {
   alert(err.message);
 });
 
-socket.on("update_room", (room) => {
-
-  if (room.owner !== socket.id) {
-     $("#start-game").remove();
+socket.on("update_room", ({ room_player, room }) => {
+  
+  if (room.owner !== Cookies.get('nickname')) {
+    $("#start_game").hide()
   };
   
+  $("#splash-screen").hide();
+  $("#start-screen").hide();
+  $("ui").show();
+  $('#gameModal').modal('hide');
+  
+  Cookies.set('code', room.code)
   code.text(room.code);
-  online.text(room.players.length)
-});
-
-$("#start-game").on("click", () => {
-  socket.emit("start_game", {
-    code: code.text(),
-  });
+  online.text(room.players.length);
+  
 });
 
 socket.on("count_down", ({ countdown }) => {
-  $(".waiting-room").remove();
+ 
+  $(".room-code").hide();
+  $(".waiting-room").hide();
   $countdown.show();
 
   if (countdown > 0) {
@@ -42,12 +80,20 @@ socket.on("count_down", ({ countdown }) => {
     countdown_text.text("GO!");
 
     setTimeout(() => {
+      $(".room-code").show();
       $countdown.remove();
       $(".game").show();
     }, 1000);
   }
+
 });
 
-socket.on("started_game", (oi) => {
-  console.log(oi);
+// Botão "Começar" no cliente
+$("#start_game").on("click", () => {
+ 
+    socket.emit("start_game", {
+      room_code: Cookies.get('code'),
+    });
+
+    
 });
