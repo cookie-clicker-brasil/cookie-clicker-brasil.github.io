371e223ce0eff0261eb7d8c03b2f389ef0b5de6a
sebastianjnuwu <sebastianjnuwu@gmail.com>
2025-03-25 22:41:47 +0000

    Deploying to gh-pages from @ cookie-clicker-brasil/cookie-clicker-brasil.github.io@b3d82d9d8b0cb4bf6664deb7e58468b85f1566b1 🚀

--- assets/index-siHKVgNR.js
+++ assets/index-siHKVgNR.js
@@ -21001,7 +21001,7 @@ function showMessage(text) {
     <div class="toast-body">${text}</div></div>`);
 }
 const socket = esmExports.io(
-  localStorage.getItem("wsURL") || void 0 || "wss://wss.squareweb.app",
+  localStorage.getItem("wsURL") || void 0 || "wss://ws.squareweb.app",
   {
     transports: ["websocket", "polling"]
   }
